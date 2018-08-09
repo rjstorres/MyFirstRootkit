5c7b165454d24a3090e969796ba3c6b039c3515d
rjstorres <r_torres96@live.com.pt>
2018-08-09 15:58:38 +0100

    why does sizeof(argv[1]) is aleays 4 :)?

--- Final/Client-side/main.c
+++ Final/Client-side/main.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    start_command(argv[1]);
+    char *input;
+    input= malloc(strlen(argv[1])*sizeof(char));
+    strncpy ( input, argv[1], sizeof(input) );
+    start_command(input);
     return 0;
 }
 
