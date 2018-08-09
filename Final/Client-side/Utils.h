6692590c3d55062687dab2e3817c480713159755
rjstorres <r_torres96@live.com.pt>
2018-08-09 12:45:05 +0100

    lets see

--- Final/Client-side/Utils.h
+++ Final/Client-side/Utils.h
@@ -11,4 +11,4 @@ int string_split(char *input, char **argument, const char delimiter);
 
 void write_buffer(char **dest_ptr, char *src, size_t size);
 
-int open_writeToFile(const char *path, const char *buf ,const size_t buf_size);
+int open_writeToFile(const char *path, char *buf ,const size_t buf_size);
--- Final/Client-side/main.c
+++ Final/Client-side/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    char input[] = "hide_file=abscdfgs";
+    char input[] = "hide_file=MyFirstRootkit";
   
     start_command(input);
     return 0;
